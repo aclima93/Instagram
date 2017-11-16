0682563f64eb705b70ebd42ba80e66fbbfa781a2
António Lima <tocalagarto93@gmail.com>
2017-11-16 17:52:00 +0000

    now displaying the username of the post's author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,13 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post was successfully created!"
       redirect_to posts_path
     else
