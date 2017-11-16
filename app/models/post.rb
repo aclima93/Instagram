cbb69fbd0b4394519f34f2168414cbeb89b2010e
António Lima <tocalagarto93@gmail.com>
2017-11-16 17:51:14 +0000

    corrected ORM

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ApplicationRecord
 
+  belongs_to :user
+
   # with every new post object that’s created we need an associated `user_id` and `image`
   validates :user_id, presence: true
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
-  belongs_to :user
-  has_many :posts, dependent: :destroy
 end
