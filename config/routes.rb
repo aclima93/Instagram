9c49a430be9b3188883c2a4a29e13a79c904ed65
António Lima <tocalagarto93@gmail.com>
2017-11-17 11:00:05 +0000

    added comments to ORM added nested route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+
+  resources :posts do
+    resources :comments
+  end
+
   root 'posts#index'
   root 'posts#new'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
 end
