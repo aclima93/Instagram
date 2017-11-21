1e43731d62f634253bced55bd3febf6a4ef1b168
António Lima <tocalagarto93@gmail.com>
2017-11-21 11:36:04 +0000

    added comment length validation added post "time ago"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,9 @@ class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
 
-  # with every new post object that’s created we need an associated `user_id` and `image`
-  validates :user_id, presence: true
+	# with every new post object that’s created we need an associated `user_id` and `image`
+	validates :user_id, presence: true
+
+	validates :content, presence: true, length: { minimum: 3, maximum: 300 }
 
 end
