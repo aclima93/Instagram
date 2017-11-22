837ecdfbb224ffc3fdaeca3f3d7780890abaea4d
António Lima <tocalagarto93@gmail.com>
2017-11-22 10:38:56 +0000

    updated registration and login forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,23 +6,23 @@ class PostsController < ApplicationController
 	# actions
 
 	def index
-	@posts = Post.all
+		@posts = Post.all
 	end
 
 	def new
-	@post = current_user.posts.build
+		@post = current_user.posts.build
 	end
 
 	def create
-	@post = current_user.posts.build(post_params)
+		@post = current_user.posts.build(post_params)
 
-	if @post.save
-		flash[:success] = "Your post was successfully created!"
-		redirect_to posts_path
-	else
-		flash.now[:alert] = "Your new post couldn't be created! Please check the form."
-		render :new
-	end
+		if @post.save
+			flash[:success] = "Your post was successfully created!"
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+			render :new
+		end
 	end
 
 	def show
@@ -32,36 +32,36 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	if @post.update(post_params)
-		flash[:success] = "Your post was successfully updated!"
-		redirect_to(posts_path(@post))
-	else
-		flash.now[:alert] = "Your new post couldn't be updated! Please check the form."
-		render :edit
-	end
+		if @post.update(post_params)
+			flash[:success] = "Your post was successfully updated!"
+			redirect_to(posts_path(@post))
+		else
+			flash.now[:alert] = "Your new post couldn't be updated! Please check the form."
+			render :edit
+		end
 	end
 
 	def destroy
-	@post.destroy
-	flash[:success] = "Your post was successfully deleted!"
-	redirect_to posts_path
+		@post.destroy
+		flash[:success] = "Your post was successfully deleted!"
+		redirect_to posts_path
 	end
 
 	private
 
 	def post_params
-	params.require(:post).permit(:image, :caption)
+		params.require(:post).permit(:image, :caption)
 	end
 
 	def set_post
-	@post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 	end
 
 	def owned_post
-	unless current_user == @post.user
-		flash[:alert] = "That post doesn't belong to you!"
-		redirect_to root_path
-	end
+		unless current_user == @post.user
+			flash[:alert] = "That post doesn't belong to you!"
+			redirect_to root_path
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	resources :posts do
-	resources :comments
-	end
+	devise_for :users, :controllers => { registrations: 'registrations' }
 
 	root 'posts#index'
 	root 'posts#new'
 
-	devise_for :users, :controllers => { registrations: 'registrations' }
+	resources :posts do
+		resources :comments
+	end
 
 end
