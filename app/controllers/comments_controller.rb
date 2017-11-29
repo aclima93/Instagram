b6fdb08eecfc654b6f4c08932e1c99dd1292343a
António Lima <tocalagarto93@gmail.com>
2017-11-29 18:59:01 +0000

    renamed "type" column to "notification_type" - it appears to be a reserved keyword -- rails should issue a warning when generating columns with reserved keywords...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,60 +1,60 @@
 class CommentsController < ApplicationController
-  before_action :set_post
+	before_action :set_post
 
-  def index
-    @comments = @post.comments.order('created_at ASC')
+	def index
+		@comments = @post.comments.order('created_at ASC')
 
-    respond_to do |format|
-      format.html { render layout: !request.xhr? }
-    end
-  end
+		respond_to do |format|
+			format.html { render layout: !request.xhr? }
+		end
+	end
 
-  def create
-    @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
+	def create
+		@comment = @post.comments.build(comment_params)
+		@comment.user_id = current_user.id
 
-    if @comment.save
+		if @comment.save
 
-      # generate a notification for the successful act of creating a comment
-      create_notification @post, @comment
+			# generate a notification for the successful act of creating a comment
+			create_notification @post, @comment
 
-      # dynamic reload with AJAX
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    else
-      flash[:alert] = 'Check the comment form, somethign went horribly wrong!'
-      render root_path
-    end
-  end
+			# dynamic reload with AJAX
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
+		else
+			flash[:alert] = 'Check the comment form, somethign went horribly wrong!'
+			render root_path
+		end
+	end
 
-  def destroy
-    @comment = @post.comments.find(params[:id])
+	def destroy
+		@comment = @post.comments.find(params[:id])
 
-    if @comment.user_id == current_user.id
-      @comment.destroy
-      # dynamic reload with AJAX
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    end
-  end
+		if @comment.user_id == current_user.id
+			@comment.destroy
+			# dynamic reload with AJAX
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
+		end
+	end
 
-  private
+	private
 
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
 
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
+	def set_post
+		@post = Post.find(params[:post_id])
+	end
 
-  def create_notification(post, comment)
-    # don't notify the current user if he owns the post
-    return if post.user.id == current_user.id
-    Notification.create(user_id: post.user.id, notified_by: current_user.id, post_id: post.id, comment_id: comment.id, type: 'comment')
-  end
+	def create_notification(post, comment)
+		# don't notify the current user if he owns the post
+		return if post.user.id == current_user.id
+		Notification.create(user_id: post.user.id, notified_by: current_user.id, post_id: post.id, comment_id: comment.id, notification_type: 'comment')
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,77 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_129_165_227) do
-  create_table 'comments', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.text 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+ActiveRecord::Schema.define(version: 20171129185424) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'notifications', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'subscribed_user_id'
-    t.integer 'post_id'
-    t.integer 'identifier'
-    t.boolean 'read'
-    t.string 'type'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_notifications_on_post_id'
-    t.index ['subscribed_user_id'], name: 'index_notifications_on_subscribed_user_id'
-    t.index ['user_id'], name: 'index_notifications_on_user_id'
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "subscribed_user_id"
+    t.integer "post_id"
+    t.integer "identifier"
+    t.boolean "read"
+    t.string "notification_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_notifications_on_post_id"
+    t.index ["subscribed_user_id"], name: "index_notifications_on_subscribed_user_id"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'caption'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'image_file_name'
-    t.string 'image_content_type'
-    t.integer 'image_file_size'
-    t.datetime 'image_updated_at'
-    t.integer 'user_id'
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'user_name'
-    t.string 'avatar_file_name'
-    t.string 'avatar_content_type'
-    t.integer 'avatar_file_size'
-    t.datetime 'avatar_updated_at'
-    t.text 'bio'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.text "bio"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'votes', force: :cascade do |t|
-    t.string 'votable_type'
-    t.integer 'votable_id'
-    t.string 'voter_type'
-    t.integer 'voter_id'
-    t.boolean 'vote_flag'
-    t.string 'vote_scope'
-    t.integer 'vote_weight'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index %w[votable_id votable_type vote_scope], name: 'index_votes_on_votable_id_and_votable_type_and_vote_scope'
-    t.index %w[voter_id voter_type vote_scope], name: 'index_votes_on_voter_id_and_voter_type_and_vote_scope'
+  create_table "votes", force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
+
 end
